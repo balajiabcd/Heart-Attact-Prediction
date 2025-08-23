4554cd39d71889281ed317669e4fb79b9efafb69
Balaji <68639299+balajiabcd@users.noreply.github.com>
2025-08-23 11:33:54 +0200

    adding code to data_utils

--- src/data_utils.py
+++ src/data_utils.py
@@ -10,28 +10,41 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     # Handle missing values
     df = df.dropna()
 
-    '''
+    # Encoding categorical variables
+    df = encode(df)
 
-Handling categorical variables: Encoding (one-hot, label encoding, target encoding).
+    # Standardization/Normalization of numerical features.
+    df = standardize_data(df)
 
-Scaling/Normalization: StandardScaler, MinMaxScaler for algorithms sensitive to scale (SVM, KNN, neural networks).
-
-Data splitting: Train-test split or cross-validation setup.
-
-Feature type conversion: Convert dates, strings, or other types into numerical or categorical formats.
-
-Removing duplicates: Ensure data is clean and non-redundant.'''
-
-
-    
-    Y = df.output
-    X = df.drop("output", axis =1)
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    # Data splitting: Train-test split or cross-validation setup.
+    X_train, X_test, y_train, y_test = split_data((df))
 
     return X_train, X_test, y_train, y_test
 
-def split_data(df: pd.DataFrame, target: str, test_size: float = 0.2, random_state: int = 42):
-    X = df.drop(target, axis=1)
-    y = df[target]
+def encode(df):
+
+    cols_to_encode = [ col for col in df.columns
+                    if df[col].nunique() > 2 and df[col].nunique() < 8]
+                    
+    df_encoded = pd.get_dummies(df, columns=cols_to_encode, prefix=cols_to_encode, prefix_sep="_")*1
+    return df_encoded
+
+
+
+
+def standardize_data(df, target_col="output"):
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+    
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    
+    X_scaled_df = pd.DataFrame(X_scaled, columns=X.columns, index=df.index)
+    result_df = pd.concat([X_scaled_df, y], axis=1)
+    return result_df
+
+def split_data(df: pd.DataFrame):
+    Y = df.output
+    X = df.drop("output", axis =1)]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
     return X_train, X_test, y_train, y_test
